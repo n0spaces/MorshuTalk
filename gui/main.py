7c859b3240fdb65067017794096f2100433c78f9
n0spaces <7495188+n0spaces@users.noreply.github.com>
2021-05-04 22:24:56 -0500

    Add slider

--- gui/main.py
+++ gui/main.py
@@ -1,23 +1,11 @@
 import sys
-import numpy as np
 import sounddevice as sd
+from PySide6.QtGui import QCloseEvent
 from pydub import AudioSegment
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 from morshu import morshu_tts
-from ui_mainwindow import Ui_MainWindow
-
-
-"""
-current_pos = 0
-def callback(in_data, frame_count, time_info, status):
-    global current_pos
-    start = current_pos
-    end = start + (frame_count * morshu_wav.channels * morshu_wav.sample_width)
-    data = raw[start:end]
-    current_pos = end
-    return data, pyaudio.paContinue
-"""
+from gui.ui_mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
@@ -30,7 +18,12 @@ class MainWindow(QMainWindow):
         self.audio_raw = self.audio_segment.raw_data
         self.samplerate = 18900
 
-        self.audio_buff_pos = 0
+        self.playing = False
+
+        self._audio_buff_pos = 0
+        self._audio_buff_end = 0
+
+        self.audio_current_time = 0
 
         # start audio stream
         self.audio_stream = sd.RawOutputStream(samplerate=self.samplerate,
@@ -38,30 +31,101 @@ class MainWindow(QMainWindow):
                                                dtype='int16',
                                                callback=self.stream_callback)
 
-        self.ui.btn_load.clicked.connect(self.btn_load_clicked)
-        self.ui.btn_play.clicked.connect(self.btn_play_clicked)
+        self.ui.btn_load.clicked.connect(self.load_audio)
+        self.ui.btn_play.clicked.connect(self.toggle_play)
+        self.ui.slider.sliderMoved.connect(self.slider_moved)
 
-    def btn_load_clicked(self):
+    """The current index position in the raw audio buffer"""
+    @property
+    def audio_buff_pos(self) -> int:
+        return self._audio_buff_pos
+
+    """
+    Sets the index in the raw audio buffer.
+    This also updates elements in the gui, like the slider (unless it's currently clicked on.)
+    """
+    @audio_buff_pos.setter
+    def audio_buff_pos(self, value):
+        self._audio_buff_pos = value
+        self.audio_current_time = self.audio_buff_pos / self.audio_segment.frame_rate / self.audio_segment.sample_width
+        self.ui.lbl_time.setText("{:.2f}".format(self.audio_current_time))
+        if not self.ui.slider.isSliderDown():
+            self.ui.slider.setValue(
+                self.audio_buff_pos // (self.audio_segment.channels * self.audio_segment.sample_width)
+            )
+
+    """Load the morshu tts with the text and update the audio fields"""
+    def load_audio(self) -> None:
+        self.playing = False
         self.audio_segment = morshu_tts(self.ui.textedit.toPlainText())
         self.audio_raw = self.audio_segment.raw_data
-        self.samplerate = self.audio_segment.frame_rate
 
-    def btn_play_clicked(self):
-        if self.audio_stream.stopped:
-            self.audio_stream.start()
+        # set audio slider length
+        self.ui.slider.setMaximum(
+            len(self.audio_raw) // (self.audio_segment.channels * self.audio_segment.sample_width)
+        )
+        self.audio_buff_pos = 0
+
+        self.ui.lbl_time_end.setText(
+            "{:.2f}".format(len(self.audio_raw) / self.audio_segment.frame_rate / self.audio_segment.sample_width))
+
+    """
+    Set the audio buffer position to the value set by the user with the slider.
+    The audio position will be equal to the value of the slider times the number of channels and the sample width of the
+    audio segment.
+    """
+    def slider_moved(self) -> None:
+        # TODO: sometimes impossible to set slider to min or max, need to fix that somehow
+        self.audio_buff_pos = self.ui.slider.value() * self.audio_segment.channels * self.audio_segment.sample_width
+
+    """Toggle the current playback"""
+    def toggle_play(self) -> None:
+        if self.playing:
+            self.stop_audio()
         else:
-            self.audio_stream.stop()
-            self.audio_buff_pos = 0
+            self.play_audio()
 
-    def stream_callback(self, outdata: bytes, frames: int, _time, _status):
-        self.ui.lbl_time.setText(str(self.audio_buff_pos / self.samplerate / self.audio_segment.sample_width))
+    """Start playing the audio. Starts the audio stream and sets the audio position to the beginning if necessary."""
+    def play_audio(self) -> None:
+        if len(self.audio_raw) > 0:
+            self.ui.btn_play.setText("Stop")
+            if self.audio_buff_pos >= len(self.audio_raw):
+                self.audio_buff_pos = 0
+            self.playing = True
+            if self.audio_stream.stopped:
+                self.audio_stream.start()
+
+    """Stop playing the audio."""
+    def stop_audio(self) -> None:
+        self.ui.btn_play.setText("Play")
+        self.playing = False
+
+    """
+    The sounddevice audio stream callback. This runs constantly while the stream is running.
+    
+    outdata should be set to the raw audio that we want to output. Its length of the audio slice must be equal to
+    frames * channels * sample_width. If we don't want to output anything, then outdata should be set to all zeros.
+    
+    Assigning the buffer data to the outdata variable won't work; use indexing instead (outdata[:] = data). 
+    """
+    def stream_callback(self, outdata: bytes, frames: int, _time, _status) -> None:
         slice_len = (frames * self.audio_segment.channels * self.audio_segment.sample_width)
 
-        if self.audio_buff_pos > len(self.audio_raw):
+        # not playing
+        if not self.playing:
             outdata[:] = b'\x00' * slice_len
-            # stopping is slow
-            self.audio_stream.stop()
-            self.audio_buff_pos = 0
+            return
+
+        # end of audio was already reached, stop playing
+        if self.audio_buff_pos >= len(self.audio_raw):
+            outdata[:] = b'\x00' * slice_len
+            self.audio_buff_pos = len(self.audio_raw)
+            self.stop_audio()
+            return
+
+        # slider is pressed, play silence and don't advance audio position but don't stop playback
+        if self.ui.slider.isSliderDown():
+            outdata[:] = b'\x00' * slice_len
             return
 
         start = self.audio_buff_pos
@@ -72,9 +136,15 @@ class MainWindow(QMainWindow):
             outdata[:] = self.audio_raw[start:end]
         self.audio_buff_pos = end
 
+    """Safely stop and close the audio stream before closing the window"""
+    def closeEvent(self, event: QCloseEvent) -> None:
+        self.audio_stream.stop()
+        self.audio_stream.close()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
 
     main_window = MainWindow()
     main_window.show()
--- gui/ui_mainwindow.py
+++ gui/ui_mainwindow.py
@@ -17,43 +17,53 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(314, 182)
+        MainWindow.resize(324, 220)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.btn_load = QPushButton(self.centralwidget)
+        self.btn_load = QToolButton(self.centralwidget)
         self.btn_load.setObjectName(u"btn_load")
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.btn_load.sizePolicy().hasHeightForWidth())
-        self.btn_load.setSizePolicy(sizePolicy)
 
         self.gridLayout.addWidget(self.btn_load, 2, 0, 1, 1)
 
-        self.btn_play = QPushButton(self.centralwidget)
+        self.btn_play = QToolButton(self.centralwidget)
         self.btn_play.setObjectName(u"btn_play")
-        sizePolicy.setHeightForWidth(self.btn_play.sizePolicy().hasHeightForWidth())
-        self.btn_play.setSizePolicy(sizePolicy)
 
         self.gridLayout.addWidget(self.btn_play, 2, 1, 1, 1)
+
+        self.lbl_time_end = QLabel(self.centralwidget)
+        self.lbl_time_end.setObjectName(u"lbl_time_end")
+
+        self.gridLayout.addWidget(self.lbl_time_end, 2, 5, 1, 1)
+
+        self.slider = QSlider(self.centralwidget)
+        self.slider.setObjectName(u"slider")
+        self.slider.setMaximum(0)
+        self.slider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout.addWidget(self.slider, 2, 2, 1, 1)
 
         self.textedit = QTextEdit(self.centralwidget)
         self.textedit.setObjectName(u"textedit")
         self.textedit.setAcceptRichText(False)
 
-        self.gridLayout.addWidget(self.textedit, 0, 0, 2, 2)
+        self.gridLayout.addWidget(self.textedit, 0, 0, 2, 6)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 2, 4, 1, 1)
 
         self.lbl_time = QLabel(self.centralwidget)
         self.lbl_time.setObjectName(u"lbl_time")
 
-        self.gridLayout.addWidget(self.lbl_time, 3, 0, 1, 2)
+        self.gridLayout.addWidget(self.lbl_time, 2, 3, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 314, 21))
+        self.menubar.setGeometry(QRect(0, 0, 324, 21))
         MainWindow.setMenuBar(self.menubar)
 
         self.retranslateUi(MainWindow)
@@ -64,7 +74,9 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MorshuTalk", None))
         self.btn_load.setText(QCoreApplication.translate("MainWindow", u"Load", None))
-        self.btn_play.setText(QCoreApplication.translate("MainWindow", u"Play/Stop", None))
-        self.lbl_time.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.btn_play.setText(QCoreApplication.translate("MainWindow", u"Play", None))
+        self.lbl_time_end.setText(QCoreApplication.translate("MainWindow", u"0.00", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"/", None))
+        self.lbl_time.setText(QCoreApplication.translate("MainWindow", u"0.00", None))
     # retranslateUi
 
