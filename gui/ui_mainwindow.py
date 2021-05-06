43bae6f84bf28377fbe0698b67a2e6cb74f16d3b
n0spaces <7495188+n0spaces@users.noreply.github.com>
2021-05-06 10:04:20 -0500

    Add morshu sprites and ui resource

--- gui/ui_mainwindow.py
+++ gui/ui_mainwindow.py
@@ -12,12 +12,13 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
+import res_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(324, 220)
+        MainWindow.resize(402, 384)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -25,45 +26,55 @@ class Ui_MainWindow(object):
         self.btn_load = QToolButton(self.centralwidget)
         self.btn_load.setObjectName(u"btn_load")
 
-        self.gridLayout.addWidget(self.btn_load, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.btn_load, 3, 0, 1, 1)
 
         self.btn_play = QToolButton(self.centralwidget)
         self.btn_play.setObjectName(u"btn_play")
 
-        self.gridLayout.addWidget(self.btn_play, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.btn_play, 3, 1, 1, 1)
 
-        self.lbl_time_end = QLabel(self.centralwidget)
-        self.lbl_time_end.setObjectName(u"lbl_time_end")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
 
-        self.gridLayout.addWidget(self.lbl_time_end, 2, 5, 1, 1)
-
-        self.slider = QSlider(self.centralwidget)
-        self.slider.setObjectName(u"slider")
-        self.slider.setMaximum(0)
-        self.slider.setOrientation(Qt.Horizontal)
-
-        self.gridLayout.addWidget(self.slider, 2, 2, 1, 1)
+        self.gridLayout.addWidget(self.label, 3, 4, 1, 1)
 
         self.textedit = QTextEdit(self.centralwidget)
         self.textedit.setObjectName(u"textedit")
         self.textedit.setAcceptRichText(False)
 
-        self.gridLayout.addWidget(self.textedit, 0, 0, 2, 6)
+        self.gridLayout.addWidget(self.textedit, 1, 0, 2, 6)
 
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
+        self.slider = QSlider(self.centralwidget)
+        self.slider.setObjectName(u"slider")
+        self.slider.setMaximum(0)
+        self.slider.setPageStep(1000)
+        self.slider.setTracking(True)
+        self.slider.setOrientation(Qt.Horizontal)
 
-        self.gridLayout.addWidget(self.label, 2, 4, 1, 1)
+        self.gridLayout.addWidget(self.slider, 3, 2, 1, 1)
+
+        self.lbl_time_end = QLabel(self.centralwidget)
+        self.lbl_time_end.setObjectName(u"lbl_time_end")
+
+        self.gridLayout.addWidget(self.lbl_time_end, 3, 5, 1, 1)
 
         self.lbl_time = QLabel(self.centralwidget)
         self.lbl_time.setObjectName(u"lbl_time")
 
-        self.gridLayout.addWidget(self.lbl_time, 2, 3, 1, 1)
+        self.gridLayout.addWidget(self.lbl_time, 3, 3, 1, 1)
+
+        self.lbl_sprite = QLabel(self.centralwidget)
+        self.lbl_sprite.setObjectName(u"lbl_sprite")
+        self.lbl_sprite.setPixmap(QPixmap(u":/sprites/store87.png"))
+        self.lbl_sprite.setScaledContents(True)
+        self.lbl_sprite.setTextInteractionFlags(Qt.NoTextInteraction)
+
+        self.gridLayout.addWidget(self.lbl_sprite, 0, 0, 1, 6)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 324, 21))
+        self.menubar.setGeometry(QRect(0, 0, 402, 21))
         MainWindow.setMenuBar(self.menubar)
 
         self.retranslateUi(MainWindow)
@@ -75,8 +86,14 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MorshuTalk", None))
         self.btn_load.setText(QCoreApplication.translate("MainWindow", u"Load", None))
         self.btn_play.setText(QCoreApplication.translate("MainWindow", u"Play", None))
-        self.lbl_time_end.setText(QCoreApplication.translate("MainWindow", u"0.00", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"/", None))
+        self.textedit.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+        self.lbl_time_end.setText(QCoreApplication.translate("MainWindow", u"0.00", None))
         self.lbl_time.setText(QCoreApplication.translate("MainWindow", u"0.00", None))
+        self.lbl_sprite.setText("")
     # retranslateUi
 
